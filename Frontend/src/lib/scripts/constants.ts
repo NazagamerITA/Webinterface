3f177bbcc17c99d6eb5210dd4e257e44a044387d
NazagamerITA <139064528+NazagamerITA@users.noreply.github.com>
2024-08-23 12:37:42 +0200

    Update constants.ts
    Signed-off-by: NazagamerITA <139064528+NazagamerITA@users.noreply.github.com>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_PATH = import.meta.env.VITE_API_URL || "https://ryzebackend.nazagamerita.com"
-export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://invite.ree6.de"
+export const BASE_PATH = import.meta.env.VITE_API_URL || "https://whee6panel.nazagamerita.com"
+export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://discord.com/oauth2/authorize?client_id=1207737052968263730"
 
 export function get(path: string) {
 
